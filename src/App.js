db31d3741894577b3ef4ce21b33bf62493adb9df
code-kumar <anilk.kopparthy@gmail.com>
2020-06-11 14:27:01 -0500

    finish project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [advice, setAdvice] = useState('');
+
+  useEffect(() => {
+    console.log('useEffect is called');
+    fetchAdvice();
+  });
+
+  async function fetchAdvice() {
+    // axios
+    //   .get('https://api.adviceslip.com/advice')
+    //   .then((response) => console.log(response.data.slip.advice))
+    //   .catch((err) => console.log(err));
+    try {
+      console.log('called the API');
+      const res = await axios.get('https://api.adviceslip.com/advice');
+      console.log(res.data.slip.advice);
+      const { advice } = res.data.slip;
+      setAdvice(advice);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='card'>
+        <h1 className='heading'>{advice}</h1>
+        <button className='button' onClick={() => fetchAdvice()}>
+          <span>GIVE ME ADVICE!</span>
+        </button>
+      </div>
     </div>
   );
 }
